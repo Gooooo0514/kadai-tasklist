c3853cd0e5747c8a390704e8611906ca041b5f3a
go.kudo <gokudo0824@gmail.com>
2021-01-30 04:57:16 +0000

    タスク管理アプリにカラムの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(5)
+        @tasks = Task.all
     end
     
     def show
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 end
 
