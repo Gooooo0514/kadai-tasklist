2a1934eb9ba2e3ab51c8b34ff6dc51fe020a3c33
go.kudo <gokudo0824@gmail.com>
2021-01-30 04:12:05 +0000

    カラム名をstatusではなくtitleとしてしまった

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_142130) do
+ActiveRecord::Schema.define(version: 2021_01_30_033937) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
